efe30947acd951b56859269f87865ce615c4591e
sairajkannan <saiboy4224@gmail.com>
2017-07-11 17:12:45 +0530

    reusing one class from another class

---
+++ Customer.java
@@ -0,0 +1,15 @@
+package payilagam.java.basics.day1;
+
+public class Customer {
+
+	public static void main(String[] args) {
+		Bank sbi=new Bank();
+		int balance = sbi.deposit(1000);
+		{
+		System.out.println(balance);
+		// TODO Auto-generated method stub
+
+	}
+
+}
+}
