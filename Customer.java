03bf5162ef241b138e3a159bf657005bfc949ec8
sairajkannan <saiboy4224@gmail.com>
2017-07-13 17:03:10 +0530

    static non static

--- Customer.java
+++ Customer.java
@@ -1,15 +1,13 @@
 package payilagam.java.basics.day1;
 
 public class Customer {
+	static String name ="sairaj";
+	private static Object mobile;
 
 	public static void main(String[] args) {
 		Bank sbi=new Bank();
-		int balance = sbi.deposit(1000);
-		{
-		System.out.println(balance);
-		// TODO Auto-generated method stub
-
-	}
-
+		sbi.CreateAccount(name);
+	
+		sbi.CreateAccount(name,mobile);
 }
 }
