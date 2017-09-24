3723bf521a9a5dd339f357912a93529e20c1d6e1
sairajkannan <saiboy4224@gmail.com>
2017-09-24 12:34:04 +0530

    Add files via upload

---
+++ StringEx.java
@@ -0,0 +1,25 @@
+
+
+
+public class StringEx
+{
+	public static void main(String[]args){
+
+	char a[] = {'s','a','i'};
+	int i,j  ;
+	for(i=2;i<3;i++)
+	{
+	
+		for(j=0;j<3;j++)
+		{
+			if(i==j)
+			System.out.print(a[i]);
+			else
+				System.out.print(" ");
+	}
+	System.out.println();
+	
+	}
+	 
+}
+}
