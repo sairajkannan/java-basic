ed8c0ae17562154ac14cc15f0a3320330d78a2cc
sairajkannan <saiboy4224@gmail.com>
2018-01-06 10:31:09 +0530

    Add files via upload

---
+++ organization.java
@@ -0,0 +1,37 @@
+package sai;
+
+public class organization {
+
+	public static void main(String[] args) {
+		int balance=500;
+//		organization org=new organization();
+		Labour org = new Labour();
+		org.getacces();
+		// TODO Auto-generated method stub
+
+	}
+
+ void getacces() {
+		System.out.println("acces");
+		// TODO Auto-generated method stub
+		
+	}
+ organization()
+ {
+	 System.out.println("hello");
+ }
+
+}
+class Labour extends organization{
+	 int balance=222;
+	 void dowork() {
+		 
+	 }
+	 
+	 
+	 Labour(){
+		 
+		 
+			 System.out.println("jkkhjlhello");
+		 }
+	 }
