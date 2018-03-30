9666221772faf01b4bc589e0de7da6c56f251e27
sairajkannan <saiboy4224@gmail.com>
2018-03-30 11:58:45 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,21 @@
+package bill;
+
+public class Function {
+	Function()
+	{
+		Function fn=new Function();
+		
+		
+	}
+	
+
+	public static void main(String[] args) {
+		B b=new B();
+		b.add();
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ cool.java
@@ -0,0 +1,35 @@
+package bill;
+import java. util. Scanner;
+public class cool {
+
+	public static void main(String[] args) {
+		
+		
+		int i,j;
+		char c[]= {'i','b','h','d','e'};
+		for(i=0;i<c.length;i++)
+		{
+			System.out.print(c[i]);
+			
+		}
+		bill(c);
+		
+	}
+	public static char[] bill(char k[])
+	{
+		char f[] = new char[k.length];
+		 int max=f[0];
+		int i=1;
+		if(max<f[i])
+		{
+			max=f[i];
+		}
+		
+		
+		
+		
+		return f;
+
+	}
+	
+}
